063d4cf6add22bbf83404cf3aad03e09a45fa5ce
Ashwin <mahesh.ashwin1998@gmail.com>
2018-07-19 00:32:38 -0700

    models.py file in clothing_dojo and clothing_admin updated

--- ClothingDojo/settings.py
+++ ClothingDojo/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'ClothingDojo.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'ClothingDojoDBNew',
+    'USER': 'root',
+    'PASSWORD': 'root',
+    'HOST': 'localhost',
+    'PORT': '3306',
     }
 }
 
--- apps/clothing_dojo/models.py
+++ apps/clothing_dojo/models.py
@@ -51,14 +51,14 @@ class User(models.Model):
     email=models.CharField(max_length=255)
     claimed_shirt=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
-    password=models.CharField(max_length=255, null=True)
+    password=models.CharField(max_length=255)
     cohort=models.ForeignKey(Cohort, related_name='students', null=True)
     location = models.ForeignKey(Location, related_name='students', null=True)
     stripe_id = models.CharField(max_length=255, null=True)
     objects=UserManager()
 
 class Cart(models.Model):
-    user=models.OneToOneField(User, primary_key=True, null=False)
+    user=models.OneToOneField(User, null=False)
     total=models.DecimalField(max_digits=9, decimal_places=2, default=0)
 
 class CartItem(models.Model):
@@ -71,7 +71,7 @@ class CartItem(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
 
 class Order(models.Model):
-    user=models.ForeignKey(User, related_name='orders', null=True)
+    user=models.ForeignKey(User, related_name='orders')
     total=models.DecimalField(max_digits=9, decimal_places=2)
     num_items=models.IntegerField(default=0)
     status=models.CharField(max_length=55, default='Unclaimed')
@@ -79,13 +79,12 @@ class Order(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     ordered=models.BooleanField(default=False)
-    batch=models.ForeignKey(Batch, related_name='orders', null=True)
+    batch=models.ForeignKey(Batch, related_name='orders')
 
 class OrderItem(models.Model):
     product=models.ForeignKey(Product, related_name='orders')
     order=models.ForeignKey(Order, related_name='items')
     size=models.CharField(max_length=3)
-    # color=models.CharField(max_length=100)
     color=models.ForeignKey(Color, related_name='order_items')
     quantity=models.IntegerField()
     total=models.DecimalField(max_digits=8, decimal_places=2, default=0)
